f64352ea409937d63a1dcb9d7fd9b1426a39eb92
LegendGhostPython <legendghostpython@gmail.com>
2026-05-18 21:52:35 -0300

    update Tiro.js

---
+++ Tiro.js
@@ -0,0 +1,24 @@
+class Tiro {
+    constructor(ctx, tiro, p) {
+        this.ctx = ctx;
+        this.largura = tiro.largura
+        this.altura = tiro.altura
+        this.cor = tiro.cor
+        this.vel = tiro.vel||7
+        this.posX =p.posX+(p.largura/2)-(this.largura/2)
+        this.posY = p.posY
+        this.dY = tiro.dY
+    }
+    update() {
+    
+    this.posY += this.vel * this.dY;
+            
+            
+    }
+    draw() {
+    this.ctx.save()
+        this.ctx.fillStyle = this.cor;
+        this.ctx.fillRect(this.posX, this.posY, this.largura, this.altura)
+        this.ctx.restore()
+    }
+}
