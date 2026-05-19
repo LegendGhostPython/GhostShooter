d301d7491d0419ad7002d8b84d594d9acc5040dc
LegendGhostPython <legendghostpython@gmail.com>
2026-05-18 21:51:48 -0300

    update Game.js

---
+++ Game.js
@@ -0,0 +1,164 @@
+/*Inicializa Variáveis*/
+var c = document.getElementById("jogo");
+var ctx = c.getContext("2d");
+var direita = document.getElementById("direita")
+var esquerda = document.getElementById("esquerda")
+var cima = document.getElementById("cima")
+var baixo = document.getElementById("baixo")
+var x = document.getElementById("x");
+var jaAtirou = false
+const teclado = {
+    dE: false,
+    dD: false,
+    dC: false,
+    dB: false
+}
+const tiro = {
+    vel:1,
+   posX: 0,
+   posY: 0,
+    quantidade: 30,
+    altura: 10,
+    largura: 5,
+    cor: "green",
+    dY:-1
+
+}
+const tiroI = {
+    vel:1,
+   posX: 0,
+   posY: 0,
+    quantidade: 30,
+    altura: 10,
+    largura: 5,
+    cor: "red",
+    dY:-1
+
+}
+const dadosP = {
+    teclado: teclado,
+    posX: 100,
+    posY: 100,
+    vel: 15,
+    largura:50,
+    altura:50,
+    dY:-1
+
+}
+
+const dadosI={
+    posX:0,
+    posY:0,
+    vel: Math.random() * 10 + 2, // Velocidade entre 2 e 7
+    largura:50,
+    altura:50,
+    cor:"red",
+    tirosPorVez:2
+    
+}
+var meustiros=[]
+var tiroinimigo=[]
+//cria Objetos/Pré carregamento
+var p = new Player(ctx, teclado, dadosP);
+var inimigo = new Enemy(ctx,dadosI)
+//desenhar
+function desenhar() {
+    meustiros.forEach(t => t.draw());
+    tiroinimigo.forEach(n => n.draw())
+    p.draw()
+    inimigo.draw()
+    ctx.save()
+    ctx.fillStyle = "blue"; // Ou a cor que preferir
+    ctx.font = "20px Arial";
+    ctx.fillText("Tiros na tela: " + meustiros.length, 10, 30);
+    ctx.fillText("Tiros na tela: " + tiroinimigo.length, 10, 50);
+    ctx.restore()
+}
+function Atualiza() {
+    let d = new Date()
+    let s = d.getSeconds()
+    p.update()
+    inimigo.update()
+    
+    meustiros.forEach(t => t.update());
+    
+    
+    // Mantém no array apenas os tiros que estão dentro da tela
+    meustiros = meustiros.filter(t => 
+        t.posY >= 0 && 
+        t.posY <= c.height && 
+        t.posX >= 0 && 
+        t.posX <= c.width
+    );
+    tiroinimigo.forEach(n=> n.update())
+   tiroinimigo = tiroinimigo.filter(n=>
+   n.posY >= 0 &&
+   n.posY <= c.height &&
+   n.posX >= 0 &&
+   n.posX <= c.width);
+   
+   if(s% 5===0 && !jaAtirou){
+       for(let j=0; j < dadosI.tirosPorVez;j++){
+           let inimigoT = new TiroInimigo(ctx,tiroI,inimigo)
+            if(tiroinimigo.length < 10){
+               tiroinimigo.push(inimigoT)
+            
+                console.log("Tiro criado em:", inimigoT.posX, inimigoT.posY);
+           }
+        }
+        jaAtirou = true
+   }
+   if( s % 5 != 0){
+       jaAtirou = false
+   }
+    
+}    
+    //controla personagem
+    direita.addEventListener("click", ()=> {
+        teclado.dD = true;
+
+    })
+    esquerda.addEventListener("click", ()=> {
+        teclado.dE = true;
+
+    })
+    cima.addEventListener("click", ()=> {
+        teclado.dC = true;
+
+    })
+    baixo.addEventListener("click", ()=> {
+        teclado.dB = true;
+
+    })
+    //ações da nave
+    x.addEventListener("click", ()=> {
+         // Definimos quantos tiros queremos disparar por clique (ex: 3)
+    const tirosPorVez = 3;
+
+    for (let i = 0; i < tirosPorVez; i++) {
+        let novoTiro = new Tiro(ctx,tiro,p)
+        // Só adiciona se o total disparado for menor que a quantidade total permitida
+        if (meustiros.length < tiro.quantidade) {
+            meustiros.push(novoTiro);
+
+        }
+    }
+    
+    })
+
+function Game() {
+    //limpa canvas
+    ctx.clearRect(0, 0, c.width, c.height)
+
+
+    //Atualiza posições e estados de Objetos
+    Atualiza()
+        //desenha Objetos
+    desenhar()
+    /*frames game*/
+    window.requestAnimationFrame(Game)
+}
+
+
+/*inicializa Game*/
+window.requestAnimationFrame(Game)
